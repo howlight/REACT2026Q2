e93701038fa9dd96592fb78ba42ae695ac39dab9
Aleksey <howlight19@gmail.com>
2026-05-04 18:59:16 +0300

    feat: implement fetch data

---
+++ src/api/rick-morty.ts
@@ -0,0 +1,32 @@
+const BASE_URL = 'https://rickandmortyapi.com/api';
+const CHARACTERS_ENDPOINT = `${BASE_URL}/character`;
+
+export type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+};
+
+const isApiResponse = (data: unknown): data is { results: unknown } =>
+  data !== null && typeof data === 'object' && 'results' in data;
+
+const isCharacterArray = (data: unknown): data is Character[] =>
+  Array.isArray(data) && data.length > 0 && 'id' in data[0] && 'name' in data[0];
+
+export const getCharacters = async (searchTerm: string): Promise<Character[]> => {
+  const url = searchTerm ? `${CHARACTERS_ENDPOINT}/?name=${searchTerm}` : `${CHARACTERS_ENDPOINT}`;
+
+  const response = await fetch(url);
+
+  if (response.status === 404) return [];
+  if (!response.ok) throw new Error('Failed to fetch characters');
+
+  const data: unknown = await response.json();
+
+  if (isApiResponse(data) && isCharacterArray(data.results)) {
+    return data.results;
+  }
+
+  throw new Error('Incorrect data structure');
+};
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,60 @@
 import { Component } from 'react';
 
+import { type Character, getCharacters } from '~/api/rick-morty';
 import { ErrorSection } from '~/components/error-section';
 import { ResultsSection } from '~/components/results-section';
 import { SearchSection } from '~/components/search-section';
 
 type AppState = {
   searchTerm: string;
+  characters: Character[];
+  loading: boolean;
+  error: null | string;
 };
 
 export class App extends Component<Record<string, never>, AppState> {
   state: AppState = {
     searchTerm: '',
+    characters: [],
+    loading: false,
+    error: null,
   };
 
   componentDidMount() {
-    const savedSearch = localStorage.getItem('search');
-
-    if (savedSearch) {
-      this.setState({ searchTerm: savedSearch });
-    }
+    void this.initializeApp();
   }
+
+  initializeApp = async () => {
+    const savedSearch = localStorage.getItem('search') ?? '';
+
+    this.setState({ searchTerm: savedSearch });
+
+    await this.fetchCharacters(savedSearch);
+  };
 
   setSearchTerm = (value: string) => {
     this.setState({ searchTerm: value });
+  };
+
+  fetchCharacters = async (searchTerm: string) => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const characters = await getCharacters(searchTerm);
+
+      this.setState({
+        characters,
+        loading: false,
+      });
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Failed to load characters';
+
+      this.setState({
+        characters: [],
+        error: errorMessage,
+        loading: false,
+      });
+    }
   };
 
   render() {
