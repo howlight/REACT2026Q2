bd85f3f1fc30b38fcb80ccc8548d6e2348bae7db
Aleksey <howlight19@gmail.com>
2026-05-10 18:01:48 +0300

    chore: setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
@@ -18,5 +20,32 @@ export default defineConfig({
     target: 'es2022',
     outDir: 'dist',
     sourcemap: false,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test-utils/setup-tests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/main.{ts,tsx}',
+        'src/test-utils/**',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+    clearMocks: true,
+    mockReset: true,
+    restoreMocks: true,
   },
 });
